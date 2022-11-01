74fed2e8c0605b445c285a7ab0aebc238c516c0d
kakekita <117062289+kakekita@users.noreply.github.com>
2022-11-01 13:26:35 +0900

    Update number.js

--- app/number.js
+++ app/number.js
@@ -5,10 +5,11 @@ function encode(n) {
     var num = String(n);
     var rand = Math.floor( Math.random() * alp.length);
     var j = code[alp[rand]].split("");
+    alert(j);
     var sp = num.split("");
     var str = "";
     for(var i in sp) {
-        str += j[Number(sp[i])]
+        str += j[Number(sp[i])];
     }
     return str;
 }
