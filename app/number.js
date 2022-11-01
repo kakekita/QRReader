bad18e9de0a7c2b5584a65f5baa144740d5392e7
kakekita <117062289+kakekita@users.noreply.github.com>
2022-11-01 13:06:28 +0900

    Update number.js

--- app/number.js
+++ app/number.js
@@ -2,7 +2,15 @@ var alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var code = null;
 
 function encode() {
-    
+    var num = String(79297);
+    var rand = Math.floor( Math.random() * alp.length);
+    var j = code[alp[rand]].split("");
+    var sp = num.split("");
+    var str = "";
+    for(var i in sp) {
+        str += j[Number(sp[i])]
+    }
+    return str;
 }
 
 $.getJSON("codes.json").done(function (json) {
