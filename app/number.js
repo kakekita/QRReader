46f537f502d4a16ee1e761beab4bc97a9dc2ebd0
kakekita <117062289+kakekita@users.noreply.github.com>
2022-11-01 12:49:03 +0900

    Update number.js

--- app/number.js
+++ app/number.js
@@ -1,4 +1,4 @@
-var alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var code = null;
 
 function encode() {
@@ -7,4 +7,6 @@ function encode() {
 
 $.getJSON("codes.json").done(function (json) {
     code = json;
+}).then(() => {
+    alert("読み込み完了");
 });
